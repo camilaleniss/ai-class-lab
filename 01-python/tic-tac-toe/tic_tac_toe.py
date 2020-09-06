f49d364c4d97064855ad008fa4c19f8850c39b49
Maria Camila Lenis Restrepo <34484913+camilaleniss@users.noreply.github.com>
2020-09-06 16:20:48 -0500

    End implementation of the is_over method

--- 01-python/tic-tac-toe/tic_tac_toe.py
+++ 01-python/tic-tac-toe/tic_tac_toe.py
@@ -7,6 +7,14 @@ _MACHINE = "machine"
 _PLAYER_SYMBOL = "x"
 _MACHINE_SYMBOL = "o"
 
+_WINNER_POSSIBILITIES = {
+  0: [[1,2], [4,8], [3, 6]],
+  1: [[4, 7]],
+  2: [[5,8], [4,6]],
+  3: [[4,6]],
+  6: [[7,8]],
+}
+
 class TicTacToeGame():
   def __init__(self):
     self.board = [None] * 9
@@ -14,7 +22,22 @@ class TicTacToeGame():
     self.is_game_over = False
     self.winner = None
 
-  def is_over(self): # TODO: Finish this function by adding checks for a winning game (rows, columns, diagonals)
+  def is_over(self): 
+    # TODO: Finish this function by adding checks for a winning game (rows, columns, diagonals)
+    for i in _WINNER_POSSIBILITIES:
+      pivot_game = _WINNER_POSSIBILITIES[i]
+      pivot_turn = self.board[i]
+
+      for item in pivot_game:
+        if pivot_turn == None:
+          break
+
+        if (self.board[item[0]] is not None and self.board[item[0]]==pivot_turn) and (self.board[item[1]] is not None and self.board[item[1]]==pivot_turn):
+          self.winner = _PLAYER if pivot_turn == _PLAYER_SYMBOL else _MACHINE
+          self.is_game_over = True
+
+          return True
+
     return self.board.count(None) == 0
 
   def play(self):
@@ -51,13 +74,11 @@ class TicTacToeGame():
     self.board[chosen_cell] = _PLAYER_SYMBOL
 
   def machine_turn(self):
-    # TODO: Implement this function to make the machine choose a random cell (use random module)
     # The result of this function should be that self.board now has one more random cell occupied
     random_cell = random.choice([x for x in range(len(self.board)) if self.board[x] is None])
     self.board[random_cell] = _MACHINE_SYMBOL
 
   def format_board(self):
-    # TODO: Implement this function, it must be able to print the board in the following format:
     board =""
 
     for i in range(len(self.board)):
@@ -80,7 +101,6 @@ class TicTacToeGame():
 
 
   def print_result(self):
-    # TODO: Implement this function in order to print the result based on the self.winner
     if self.winner is None:
       print("It was a draw, better luck next time")
     else:
